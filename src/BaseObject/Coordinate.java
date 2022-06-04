15f40249c10ade852d90fa549d3d5ff767f89bbd
ACMLCZH <965503066@qq.com>
2022-06-04 19:24:37 +0800

    add Coordinate to public

---
+++ src/BaseObject/Coordinate.java
@@ -0,0 +1,21 @@
+package BaseObject;
+
+public class Coordinate implements Comparable<Coordinate>
+{
+	public int x, y;
+
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int compareTo(Coordinate o) {
+        int r = this.y - o.y;
+        if (r == 0)
+            return this.x - o.x;
+		return r;
+	}
+
+	public boolean equals(Coordinate o) {return this.x == o.x && this.y == o.y;}
+};
