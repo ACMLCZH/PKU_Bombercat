b4a62ef9c3406b51c4367016b9d52dc68340f219
ACMLCZH <965503066@qq.com>
2022-06-05 16:31:39 +0800

    Update HumanPlayer.java

--- src/BasePlayer/HumanPlayer.java
+++ src/BasePlayer/HumanPlayer.java
@@ -1,12 +1,13 @@
 package BasePlayer;
 
 import BaseObject.Coordinate;
+import main.Game;
 
 public class HumanPlayer extends BasePlayer
 {
 	public static final int INIT_HP = 10000;
-	public HumanPlayer(int HP, Coordinate spawn, Indirect dir, String name)
+	public HumanPlayer(int HP, Coordinate spawn, Indirect dir, String name, Game game)
 	{
-		super(HP, spawn, dir, name);
-	}    
+		super(HP, spawn, dir, name, game);
+	}
 }
