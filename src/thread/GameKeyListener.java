a1bd0acd32b8d18afc31f7806e3f0f8eceb0b2c1
ACMLCZH <965503066@qq.com>
2022-06-09 20:44:02 +0800

    Update GameKeyListener.java

--- src/thread/GameKeyListener.java
+++ src/thread/GameKeyListener.java
@@ -1,7 +1,5 @@
 package thread;
 
-import static DEBUG.Dbg.msg;
-
 import BasePlayer.HumanPlayer;
 import BasePlayer.Indirect;
 import main.Game;
@@ -15,14 +13,12 @@ public class GameKeyListener implements KeyListener
     private Stack<Integer> pressStack = new Stack<>();
 	private Game g = null;
 	private HumanPlayer player = null;
-    //private List<SuperObject> playerList;
 
 	public GameKeyListener(Game g) {this.g = g;}
 	
     @Override
     public void keyPressed(KeyEvent e)
 	{
-		// msg("?????????");
         int code = e.getKeyCode();
         if (player.isAlive())
 		{
@@ -49,8 +45,6 @@ public class GameKeyListener implements KeyListener
     @Override
     public void keyReleased(KeyEvent e)
 	{
-    	// HumanPlayer player = g.getInfoPlayer(); // 想取出玩家指针，这句话咋写啊，不会
-		// msg("???!!");
         int code = e.getKeyCode();
         if (player.isAlive())
 		{
@@ -58,9 +52,7 @@ public class GameKeyListener implements KeyListener
 			{
                 case 32:
 					g.commandQueue.add(() -> {player.release();});
-                    break; //
-                    // player.setKeepAttack(false);
-                    // player.setAttack(false);
+                    break;
                 case 37: case 38: case 39: case 40:
                 case 87: case 65: case 68: case 83:
 					if (pressStack.peek() == code)
